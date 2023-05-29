12d71c83231a924aca06415b678a6e2c9bdedac0
Goldwayne Leh <goldwayneleh@goldwaynes-mbp.lan>
2023-05-29 02:22:35 -0700

    fixing contact form

---
+++ src/layout/ContactLayout.tsx
@@ -0,0 +1,66 @@
+import { ReactNode } from 'react';
+
+type IContactLayoutProps = {
+  yPadding?: string;
+  children: ReactNode;
+};
+
+const ContactLayout = (props: IContactLayoutProps) => (
+  <div
+    className={`max-w-screen-lg mx-auto px-3 ${
+      props.yPadding ? props.yPadding : 'py-16'
+    }`}
+  >
+    <div className="mb-12 text-center">
+      <h2 className="text-4xl text-gray-900 font-bold">Contact Us</h2>
+        <div className="mt-4 text-xl md:px-20">If you're interested in helping us with our dreams, please reach out.</div>
+    </div>
+
+    <div className="flex flex-col items-center justify-center">
+      <form className="w-full max-w-sm">
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
+              Full Name
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary-500" id="inline-full-name" type="text" placeholder="Jane Doe"/>
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-email">
+              Email
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary-500" id="inline-email" type="text" placeholder="example@gmail.com"/>
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-message">
+              Message
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary-500" id="inline-message" placeholder="Hello World!"/>
+          </div>
+        </div>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <button className="shadow bg-primary-500 hover:bg-primary-600 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    {props.children}
+  </div>
+);
+
+export { ContactLayout };
--- src/templates/ContactForm.tsx
+++ src/templates/ContactForm.tsx
@@ -1,26 +1,8 @@
-import { Section } from '../layout/Section';
+import { ContactLayout } from '../layout/ContactLayout';
 
 const ContactForm = () => (
-  <Section
-    title="Contact Us"
-    description="If you're interested in helping us with our dreams, please reach out."
-  >
-    <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-
-  </Section>
+  <ContactLayout>
+  </ContactLayout>
 );
 
 export { ContactForm };
