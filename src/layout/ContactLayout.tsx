1197b8f9be393ddf9b704f17e2040289634a9bb0
Goldwayne Leh <goldwayneleh@goldwaynes-mbp.lan>
2023-05-29 02:46:09 -0700

    adding hidden input

--- src/layout/ContactLayout.tsx
+++ src/layout/ContactLayout.tsx
@@ -18,6 +18,8 @@ const ContactLayout = (props: IContactLayoutProps) => (
 
     <div className="flex flex-col items-center justify-center">
       <form className="w-full max-w-sm" name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="contact" value="contact" />
+
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">
             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
